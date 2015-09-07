92d4bc82f4c9e2bbe573612cf090c3ed32d6e68f
Vincent Kramar <thkrmr@users.noreply.github.com>
2015-09-07 14:14:40 +0200

    package.pp switch to the original danilop/yas3fs

--- manifests/package.pp
+++ manifests/package.pp
@@ -32,7 +32,7 @@ class yas3fs::package {
     ensure        => present,
     provider      => 'pip',
     allow_virtual => true,
-    source        => 'git+git://github.com/padde/yas3fs.git@s3-sigv4',
+    source        => 'git+git://github.com/danilop/yas3fs@master',
     require       => [$fuse_req, $pip_req],
   }
 
